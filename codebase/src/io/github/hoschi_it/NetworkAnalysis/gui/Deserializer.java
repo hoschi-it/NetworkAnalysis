5286e14a9bc55bc8a773b581d8e72da40446893e
Tufan Tavasolli Aghdam <tufan.tavasolli@gmail.com>
2019-12-09 13:10:28 +0100

    updated Deserializer

--- codebase/src/io/github/hoschi_it/NetworkAnalysis/gui/Deserializer.java
+++ codebase/src/io/github/hoschi_it/NetworkAnalysis/gui/Deserializer.java
@@ -1,27 +1,68 @@
-package io.github.hoschi_it.NetworkAnalysis.gui;
+package gui;
 
 import java.io.FileReader;
-import java.io.Reader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Deserializer {
 
-    public static void main(String[] args) {
-        System.out.println("TEST");
-        CSVParse("C:/Users/uidn4922/Desktop/test.csv");
-    }
-    public static void CSVParse(String csvfilepath) {
-        String[] oneLine;
-        Reader reader = new FileReader((csvfilepath));
-        CSVReader csvReader = new CSVReader(reader, ';');
+        public static void main(String[] args) {
+            List<Integer[]> data = csvRead();
+            for (Integer[] list : data){
+                for(Integer value: list){
 
-        int i = 0;
-        while ((oneLine = csvReader.readNext()) != null) {
-                System.out.println("Read line: " + i);
-                for (String elem : oneLine) {
-                    System.out.println("Elem: " + elem);
+                    System.out.print(value + " ");
                 }
+                System.out.println("\n");
+            }
         }
+    public static List<Integer[]> csvRead(){
+        String path = "C:/Users/uidn4922/Desktop/test.csv";
+        return csvRead(path);
     }
+    public static List<Integer[]> csvRead (String path){
+            String csvFile = path;
+            BufferedReader br = null;
+            String line = "";
+            String cvsSplitBy = ";";
+            List<Integer[]> ajd = new ArrayList<Integer[]>();
+            try {
+
+                br = new BufferedReader(new FileReader(csvFile));
+                while ((line = br.readLine()) != null) {
+
+                    if (!line.contains("#")){
+                        // use semicolon as separator
+                        String[] values = line.split(cvsSplitBy);
+                        Integer[] valuesInt = new Integer[values.length];
+                        int i =0;
+                        for(String value: values){
+                            valuesInt[i] = Integer.parseInt(value);
+                            i++;
+                        }
+                        ajd.add(valuesInt);
+                    }
 
 
-}
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        return ajd;
+        }
+
+    }
