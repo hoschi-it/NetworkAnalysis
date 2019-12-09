4a482d2daa606d44e383ec617e1cc39c5cb22cec
Janina Hosch <hoschi-it@web.de>
2019-12-09 12:22:36 +0100

    add Dimis Adjacenzmatrix

--- codebase/src/io/github/hoschi_it/NetworkAnalysis/data/AdjacenzMatrix.java
+++ codebase/src/io/github/hoschi_it/NetworkAnalysis/data/AdjacenzMatrix.java
@@ -1,5 +1,31 @@
 package io.github.hoschi_it.NetworkAnalysis.data;
 
-public class AdjacenzMatrix
-{
+import java.util.List;
+
+public class AdjacenzMatrix {
+
+    private List<Integer[]> matrix;
+    private int columnCount;
+    private int rowCount;
+
+    public int getColumnCount() {
+        return columnCount;
+    }
+    public int getRowCount() {
+        return rowCount;
+    }
+    public List<Integer[]> getMatrix(){
+        return this.matrix;
+    }
+
+    public AdjacenzMatrix() {}
+
+    public AdjacenzMatrix(List<Integer[]> matrix) {
+        this.matrix = matrix;
+        this.columnCount = matrix.get(0).length;
+        this.rowCount = matrix.size();
+    }
+
 }
+
+
