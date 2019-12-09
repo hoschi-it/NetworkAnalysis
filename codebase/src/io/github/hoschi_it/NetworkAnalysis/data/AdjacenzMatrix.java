13b2bfb39ccc5c742d548fd66b70d862846348b8
Dimi1991 <dimitri.khodak@continental.com>
2019-12-09 12:12:08 +0100

    Einfügen der Adjazenzmatrix Klasse

--- codebase/src/io/github/hoschi_it/NetworkAnalysis/data/AdjacenzMatrix.java
+++ codebase/src/io/github/hoschi_it/NetworkAnalysis/data/AdjacenzMatrix.java
@@ -1,5 +1,28 @@
 package io.github.hoschi_it.NetworkAnalysis.data;
 
-public class AdjacenzMatrix
-{
+public class Adjazenzmatrix {
+
+	private List<int[]> matrix;
+	private int columnCount;
+	private int rowCount;
+	
+	public int getColumnCount() {
+		return columnCount;
+	}
+	public int getRowCount() {
+		return rowCount;
+	}
+	public List<int[]> getMatrix(){
+		return this.matrix;
+	}
+	
+	public Adjazenzmatrix() {}
+	
+	public Adjazenzmatrix(List<int[]> matrix) {
+		this.matrix = matrix;
+		this.columnCount = matrix.get(0).length;
+		this.rowCount = matrix.size();
+	}
+	
 }
+
