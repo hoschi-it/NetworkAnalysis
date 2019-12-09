fab746280c7919c9cf9e7b1bdd8d0685ee471f44
Janina Hosch <hoschi-it@web.de>
2019-12-09 11:38:19 +0100

    write data classes

--- codebase/src/io/github/hoschi_it/NetworkAnalysis/data/Connection.java
+++ codebase/src/io/github/hoschi_it/NetworkAnalysis/data/Connection.java
@@ -1,5 +1,32 @@
 package io.github.hoschi_it.NetworkAnalysis.data;
 
+/**
+ * Directional connection between two nodes in a Graph
+ */
 public class Connection
 {
+    private int _fromNodeId;
+    private int _toNodeId;
+    private float _weight;
+
+    public Connection(int fromNodeId, int toNodeId, float weight){
+        _fromNodeId = fromNodeId;
+        _toNodeId = toNodeId;
+        _weight = weight;
+    }
+
+    public int getFromNodeId()
+    {
+        return _fromNodeId;
+    }
+
+    public int getToNodeId()
+    {
+        return _toNodeId;
+    }
+
+    public float getWeight()
+    {
+        return _weight;
+    }
 }
--- codebase/src/io/github/hoschi_it/NetworkAnalysis/data/Node.java
+++ codebase/src/io/github/hoschi_it/NetworkAnalysis/data/Node.java
@@ -2,4 +2,22 @@ package io.github.hoschi_it.NetworkAnalysis.data;
 
 public class Node
 {
+    private int _innerDegree;
+    private float _betweennes;
+    private int _closenes;
+
+    public void setInnerDegree(int innerDegree){
+        _innerDegree = innerDegree;
+    }
+
+    public void setBetweennes(float betweennes)
+    {
+        _betweennes = betweennes;
+    }
+
+    public void setClosenes(int closenes)
+    {
+        _closenes = closenes;
+    }
+
 }
