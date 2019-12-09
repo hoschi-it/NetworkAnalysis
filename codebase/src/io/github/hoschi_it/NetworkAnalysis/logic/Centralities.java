decc1f1ec53dd7dafffdef3664a25fd6177b3241
Janina Hosch <hoschi-it@web.de>
2019-12-09 12:15:05 +0100

    implement calculating innerDegree

--- codebase/src/io/github/hoschi_it/NetworkAnalysis/logic/Centralities.java
+++ codebase/src/io/github/hoschi_it/NetworkAnalysis/logic/Centralities.java
@@ -1,5 +1,20 @@
 package io.github.hoschi_it.NetworkAnalysis.logic;
 
+
+import io.github.hoschi_it.NetworkAnalysis.data.Graph;
+
+import java.util.ArrayList;
+
 public class Centralities
 {
+    public ArrayList<Integer> calculateInnerDegrees(Graph graph){
+        ArrayList<Integer> innerDegrees = new ArrayList<>();
+
+        for(int id: graph.getNodeIds()){
+            int degree = graph.getConnectionsTo(id).size();
+            innerDegrees.add(degree);
+        }
+
+        return innerDegrees;
+    }
 }
