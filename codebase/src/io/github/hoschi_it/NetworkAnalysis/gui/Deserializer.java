87ab24f338cd2f3c8853ebdc60b15f4a6029c792
Tufan Tavasolli Aghdam <tufan.tavasolli@gmail.com>
2019-12-09 13:04:16 +0100

    deserilizer

--- codebase/src/io/github/hoschi_it/NetworkAnalysis/gui/Deserializer.java
+++ codebase/src/io/github/hoschi_it/NetworkAnalysis/gui/Deserializer.java
@@ -1,5 +1,27 @@
 package io.github.hoschi_it.NetworkAnalysis.gui;
 
-public class Deserializer
-{
+import java.io.FileReader;
+import java.io.Reader;
+
+public class Deserializer {
+
+    public static void main(String[] args) {
+        System.out.println("TEST");
+        CSVParse("C:/Users/uidn4922/Desktop/test.csv");
+    }
+    public static void CSVParse(String csvfilepath) {
+        String[] oneLine;
+        Reader reader = new FileReader((csvfilepath));
+        CSVReader csvReader = new CSVReader(reader, ';');
+
+        int i = 0;
+        while ((oneLine = csvReader.readNext()) != null) {
+                System.out.println("Read line: " + i);
+                for (String elem : oneLine) {
+                    System.out.println("Elem: " + elem);
+                }
+        }
+    }
+
+
 }
